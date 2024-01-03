0c21fd7133a2d33cdf2f561a4c98769a0563a65e
nileshswami1003 <s.nilesh1003@gmail.com>
2024-01-03 18:10:09 +0530

    Setup Admin & User Login with Middleware & redirect | multi auth

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Support\Facades\Auth;
+
 
 class LoginController extends Controller
 {
@@ -26,7 +28,22 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = RouteServiceProvider::HOME;
+    // protected $redirectTo = RouteServiceProvider::HOME;
+    public function authenticated()
+    {
+        if(Auth::user()->role_as=='1') //1->admin user
+        {
+            return redirect('admin/dashboard')->with('status','Welcome to admin dashboard');
+        }
+        else if(Auth::user()->role_as=='0') //0->normal user
+        {
+            return redirect('/home')->with('status','Logged in successfully');
+        }
+        else
+        {
+            return redirect('/');
+        }
+    }
 
     /**
      * Create a new controller instance.
